bba63085f23eb988e8591353adbfa75f6062b1f7
Tim <github@tpillard.fr>
2020-09-18 20:15:57 +0200

    Add storage module
    Store items in local storage.
    
    The items collections is stored in the `items.all` namespace, under the
    form of an object of shape { [id: string]: (title: string), ... }
    
    The individual items are stored in their own namespace of the form
    `items.${id}` and of value `content: string`, content being the
    encrypted output of the item's real content.
    
    Following methods are exported:
    - getAllItems()
    - getItem(id)
    - deleteItem(id)
    - saveItem(id, title, content)

---
+++ src/modules/storage.js
@@ -0,0 +1,43 @@
+const namespace = 'items';
+
+/*
+ * items.all : { [id]: title, ... }
+ * items.${id} : content
+ */
+
+export function getAllItems() {
+  const stored = window.localStorage.getItem(`${namespace}.all`);
+  if (!stored) {
+    return [];
+  }
+
+  const items = JSON.parse(stored);
+  return Object.keys(items).map((id) => ({ id, title: items[id] }));
+}
+
+export function getItem(id) {
+  return window.localStorage.getItem(`${namespace}.${id}`);
+}
+
+export function deleteItem(id) {
+  return window.localStorage.removeItem(`${namespace}.${id}`);
+}
+
+export function saveItem({ id, title, content }) {
+  const items = getAllItems();
+  const storable = items.reduce((acc, item) => {
+    acc[item.id] = item.title;
+    return acc;
+  }, {});
+
+  window.localStorage.setItem(
+    `${namespace}.all`,
+    JSON.stringify({ ...storable, [id]: title })
+  );
+  try {
+    window.localStorage.setItem(`${namespace}.${id}`, content);
+  } catch (err) {
+    window.localStorage.setItem(`${namespace}.all`, JSON.stringify(storable));
+    throw err;
+  }
+}
