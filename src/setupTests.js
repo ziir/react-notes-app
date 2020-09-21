478bff230be6a4b735744620ee4ebcf51538d209
Tim <github@tpillard.fr>
2020-09-21 08:38:16 +0200

    Shim CE API to unbreak test setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,4 +4,4 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 
-global.customElements = {};
+global.customElements = { get: () => {}, define: () => {} };
